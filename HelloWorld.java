92f00fa27e7dc76265721346977f7c9734c630fd
UjwalE <ujwal.vssut@gmail.com>
2018-11-22 19:14:31 +0530

    Test Auto Build

--- HelloWorld.java
+++ HelloWorld.java
@@ -9,7 +9,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		
-		System.out.println("This is a Sample program");
+		System.out.println("This is a Sample program For Git Integration");
 
 	}
 
